16f181990602c45ec23aaf7e6b9c5c27d68507b8
MchllD <mich11dimafelix@gmail.com>
2024-01-27 23:31:32 +0800

    pseudocode

---
+++ Exercise15.py
@@ -0,0 +1,15 @@
+# Exercise 15: Write a function called exponent(base, exp) that returns an int value of base raises to the power of exp.
+# Note here exp is a non-negative integer, and the base is an integer.
+
+# pseudocode
+
+# create function to calculate the exponent of a given base and exponent
+# Initialize the result to 1
+# Loop through the range of the exponent
+# Multiply the result by the base in each iteration
+
+# Test cases
+# Set values for the first test case
+
+# ---------------------------------------- actual code ---------------------------------------------------
+
